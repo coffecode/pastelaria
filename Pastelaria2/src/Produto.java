5929eebf461715b6bfdc8e8e14e122f48e6d68b6
fernando <fernando@fernando>
2013-12-19 15:08:50 -0200

    Falta coisas

--- Pastelaria2/src/Produto.java
+++ Pastelaria2/src/Produto.java
@@ -6,5 +6,34 @@ public class Produto {
 	private int quantidade; //quantidade de produtos vendidos
 	
 	private Double preco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	
+	
+	
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 	
 }
--- Pastelaria2/src/Venda.java
+++ Pastelaria2/src/Venda.java
@@ -1,4 +1,29 @@
 
-public class Venda {
 
+public class Venda {
+		private Produto[] produtos; //Produtos que terão na venda
+		
+		private int quantidadeVenda; //Quantidade de vendas
+		private int quantidadeProdutos;
+		private double preco;  //Preço total dessa venda
+		
+		public Venda() {
+			this.produtos = new Produto[30]; //Inicializo meus produtos com 30 produtos
+										//Acho um tamanho bom, qualquer coisa é só alterar
+			this.quantidadeProdutos = 0;
+			this.quantidadeVenda = 0;
+
+			this.preco = 0;
+			
+
+		}
+		
+		public void adicionarProduto(String nome, int quantidade) {
+			this.produtos[this.quantidadeProdutos] = new Produto();
+			this.produtos[this.quantidadeProdutos].setNome(nome);
+			this.produtos[this.quantidadeProdutos].setQuantidade(quantidade);
+			this.preco +=  this.produtos[this.quantidadeProdutos].getPreco()  * quantidade;
+			
+			this.quantidadeProdutos++;
+		}
 }
