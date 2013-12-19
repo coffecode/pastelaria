773da8b11fed61962408e3b988047ab015c9aaad
fernando <fernando@fernando>
2013-12-19 01:26:45 -0200

    mesa commit 2

--- Pastelaria2/src/Mesa.java
+++ Pastelaria2/src/Mesa.java
@@ -1,4 +1,16 @@
-
 public class Mesa {
-
+	int nMesa;   //Cada mesa terá o seu numero
+	double valorNaoPago;
+	double ValorPago;
+	double valorTotalGasto; //valorTotalGasto = valorPago + valorNaoPago
+	public Mesa(int numero){
+		this.nMesa = numero;
+		
+		this.valorNaoPago = 0;
+		this.ValorPago = 0;
+		this.valorTotalGasto =0;
+	}
+	
+	
+	
 }
