983b24711eb371a4fc6d8b3bafb9f46f855cd48c
fernando <fernando@fernando>
2013-12-20 16:03:21 -0200

    Parte modal pronta (eu acho)

--- Pastelaria2/src/Mesa.java
+++ Pastelaria2/src/Mesa.java
@@ -4,7 +4,9 @@ public class Mesa {
 	private double valorNaoPago;
 	private double ValorPago;
 	private double valorTotalGasto; //valorTotalGasto = valorPago + valorNaoPago
+	private int quantidade; //quantidade de vendas
 	
+	private Venda[] vendas = new Venda[50];
 	
 	public Mesa(int numero){
 		this.numero = numero;
@@ -13,6 +15,67 @@ public class Mesa {
 		this.ValorPago = 0;
 		this.valorTotalGasto =0;
 	}
+	
+	public void novaVenda(){
+		this.vendas[this.quantidade] = new Venda();
+		this.quantidade++;
+		this.calcularPreco();
+
+	}
+	
+	public void excluirVenda(int numero, int quantidade){
+		//Caso o nomero de vendas a serem excluidos seja maior do que a quantidade do mesmo produto
+		//eu excluo toda ela do array de vendas e reorganizo o array 
+		if(this.vendas[numero].getQuantidadeVenda()<= quantidade){
+			this.quantidade --;
+			for(int i=numero; i<this.quantidade; i++){
+				this.vendas[i] = this.vendas[i+1];
+			}
+			this.vendas[this.quantidade +1] = null;
+		}else{
+			//Caso a quantidade de itens a serem excluidos seja menor,
+			//apenas diminuo isso na sua quantidade
+			this.vendas[numero].setQuantidadeVenda(quantidade, 2);
+			
+		}
+		this.calcularPreco();
+
+	}
+	
+	public void excluirProdutoEmUmaVenda(String nome, int nVenda){
+		this.vendas[nVenda].removeProduto(nome);
+		this.calcularPreco();
+	}
+	
+	public void adicionarProdutoEmUmaVenda(String nome, int nVenda){
+		this.vendas[nVenda].aumentaQuantidadeProduto(nome);
+		this.calcularPreco();
+	}
+	
+	public void diminuiProdutoEmUmaVenda(String nome, int nVenda){
+		this.vendas[nVenda].diminuiQuantidadeProduto(nome);
+		this.calcularPreco();
+
+	}
+	public void pagarVenda(int nVenda){
+		this.vendas[nVenda].setPago(1);		
+		this.calcularPreco();
+	}
+	
+	private void calcularPreco(){
+		this.valorNaoPago = 0;
+		this.ValorPago = 0;
+		this.valorTotalGasto = 0;
+		for(int i=0; i<this.quantidade; i++){
+			if(this.vendas[i].getPago() == 0){
+				this.valorNaoPago+= this.vendas[i].getPreco();
+			}else{
+				this.ValorPago += this.vendas[i].getPreco();
+			}
+		}
+		this.valorTotalGasto = this.valorNaoPago + this.ValorPago;
+	}
+	
 	
 	
 	
--- Pastelaria2/src/Venda.java
+++ Pastelaria2/src/Venda.java
@@ -4,33 +4,32 @@ public class Venda {
 		private Produto[] produtos; //Produtos que terão na venda
 		
 		private int quantidadeVenda; //Quantidade de vendas
-		private int quantidadeProdutos;
+		private int quantidadeProdutos; //Quantidade de produtos nessa venda 
 		private double preco;  //Preço total dessa venda
-
-		private int quantidadeProduto;
+		private int pago;
+		
 		
 		public Venda() {
 			this.produtos = new Produto[30]; //Inicializo minha venda com 30 produtos(null)
 										//Acho um tamanho bom, qualquer coisa é só alterar
 			this.quantidadeProdutos = 0;
-			this.quantidadeVenda = 0;
-
-			this.preco = 0;
+			this.quantidadeVenda= 0;
+			this.setPago(0);
+			this.setPreco(0);
 			
 
 		}
 		
-		public void adicionarProduto(String nome, int quantidade) {
+		public void adicionarProduto(String nome) {
 			this.produtos[this.quantidadeProdutos] = new Produto();
 			this.produtos[this.quantidadeProdutos].setNome(nome);
-			this.produtos[this.quantidadeProdutos].setQuantidade(quantidade, 0);
-			this.preco +=  this.produtos[this.quantidadeProdutos].getPreco()  * quantidade;
-			
+			this.produtos[this.quantidadeProdutos].setQuantidade(1, 0);
 			this.quantidadeProdutos++;
+			this.calculaPreco();
 		}
 		
 		
-		public void removeProduto(String nome, int quantidade){
+		public void removeProduto(String nome){
 			
 			int produtoId =0;
 			
@@ -42,20 +41,14 @@ public class Venda {
 				}
 			}
 			
-			//Caso o nomero de produtos a serem excluidos seja maior do que a quantidade do mesmo produto
 			//eu excluo todo ele do array de produtos e reorganizo o array 
-			if(this.produtos[produtoId].getQuantidade()<= quantidade){
-				this.quantidadeProdutos --;
-				for(int i=produtoId; i<this.quantidadeProdutos; i++){
-					this.produtos[i] = this.produtos[i+1];
-				}
-				this.produtos[this.quantidadeProduto +1] = null;
-			}else{
-				//Caso a quantidade de itens a serem excluidos seja menor,
-				//apenas diminuo isso na sua quantidade
-				this.produtos[produtoId].setQuantidade(quantidade, 2);
-				
+			this.quantidadeProdutos --;
+			for(int i=produtoId; i<this.quantidadeProdutos; i++){
+				this.produtos[i] = this.produtos[i+1];
 			}
+			this.produtos[this.quantidadeProdutos +1] = null;
+			
+			this.calculaPreco();
 		}
 		
 		public void aumentaQuantidadeProduto(String nome) {
@@ -70,6 +63,77 @@ public class Venda {
 			}
 			
 			this.produtos[produtoId].setQuantidade(1, 1);
+			this.calculaPreco();
 		}
 		
+		public void diminuiQuantidadeProduto(String nome) {
+			int produtoId =0;
+			
+			//Salvo o offset do produto no array, usando o nome para pesquisar no array
+			for(int i=0; i<this.quantidadeProdutos; i++){
+				if(this.produtos[i].getNome().equals(nome)){
+					produtoId = i;
+					break;
+				}
+			}
+			
+			this.produtos[produtoId].setQuantidade(1, 2);
+			this.calculaPreco();
+		}
+
+		public int getQuantidadeVenda() {
+			return quantidadeVenda;
+		}
+
+		public void setQuantidadeVenda(int quantidade, int tipo) {
+			//O = Substitue
+			//1 = Soma
+			//2 = subtrai
+			//3 = multiplica
+			//4 = divide
+			
+			switch (tipo) {
+			case 0:
+				this.quantidadeVenda = quantidade;
+				break;
+
+			case 1:
+				this.quantidadeVenda += quantidade;
+				break;
+			case 2:
+				this.quantidadeVenda -= quantidade;
+				break;
+			case 3:
+				this.quantidadeVenda = this.quantidadeVenda* quantidade;
+				break;
+			case 4:
+				this.quantidadeVenda = this.quantidadeVenda/ quantidade;
+				break;	
+			default:
+				break;
+			}
+			this.calculaPreco();
+		}
+		private void calculaPreco(){
+			this.setPreco(0);
+			for(int i=0; i< this.produtos.length; i++){
+				this.setPreco(this.getPreco() + this.produtos[i].getPreco() * this.produtos[i].getQuantidade()); 
+			}
+		}
+
+		public int getPago() {
+			return pago;
+		}
+
+		public void setPago(int pago) {
+			this.pago = pago;
+		}
+
+		public double getPreco() {
+			return preco;
+		}
+
+		public void setPreco(double preco) {
+			this.preco = preco;
+		}
 }
