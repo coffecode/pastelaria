e554e3f66b85c105cd46b064495936b68cd2fe8d
andre <andre@192.168.1.2>
2013-12-31 19:53:48 -0200

    up

--- Pastelaria2/src/MenuLogin.java
+++ Pastelaria2/src/MenuLogin.java
@@ -29,7 +29,7 @@ public class MenuLogin extends JFrame implements ActionListener
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
+		} catch (UnsupportedLookAndFeelException e) {//
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
